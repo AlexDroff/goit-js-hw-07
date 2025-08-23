09b11284ca8466042f03938a6b1efed3410c97e0
Oleksandr Aleksandrov <alldroff@gmail.com>
2025-08-23 15:39:59 +0200

    finish2

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
 
 inputEl.addEventListener("change", (event) => {
-  const value = event.currentTarget.value.trim();
+  const value = event.target.value.trim();
   const displayName = value === "" ? "Anonymous" : value;
   outputEl.textContent = displayName;
   console.log(`Hello, ${displayName}!`);
